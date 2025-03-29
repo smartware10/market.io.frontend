1ae40d688f7fc698b904c2d62bde0fe157ffc81a
Roman 4im <smartware10@gmail.com>
2025-03-29 09:52:02 +0200

    Edit logo and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { nunito } from "@/fonts";
+import Favicon from "/public/logo.svg";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { Header } from "@/components/shared";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Маркет АйОу",
   description: "",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
