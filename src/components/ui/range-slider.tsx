6e879bf71c944824c77240d874af42926d05fd59
Roman 4im <smartware10@gmail.com>
2025-03-31 15:31:00 +0300

    Edit range-slider

--- src/components/ui/range-slider.tsx
+++ src/components/ui/range-slider.tsx
@@ -10,18 +10,20 @@ type SliderProps = {
   min: number;
   max: number;
   step: number;
+  hideValue?: boolean;
   formatLabel?: (value: number) => string;
   value?: number[] | readonly number[];
   onValueChange?: (values: number[]) => void;
 };
 
-const RangeSlider = React.forwardRef(
+const RangeSlider = React.forwardRef<HTMLDivElement, SliderProps>(
   (
     {
       className,
       min,
       max,
       step,
+      hideValue = false,
       formatLabel,
       value,
       onValueChange,
@@ -33,9 +35,10 @@ const RangeSlider = React.forwardRef(
     const [localValues, setLocalValues] = React.useState(initialValue);
 
     React.useEffect(() => {
-      // Update localValues when the external value prop changes
-      setLocalValues(Array.isArray(value) ? value : [min, max]);
-    }, [min, max, value]);
+      if (value !== undefined) {
+        setLocalValues(Array.isArray(value) ? value : [min, max]);
+      }
+    }, [value, min, max]);
 
     const handleValueChange = (newValues: number[]) => {
       setLocalValues(newValues);
@@ -46,35 +49,38 @@ const RangeSlider = React.forwardRef(
 
     return (
       <SliderPrimitive.Root
-        ref={ref as React.RefObject<HTMLDivElement>}
+        ref={ref}
         min={min}
         max={max}
         step={step}
         value={localValues}
         onValueChange={handleValueChange}
         className={cn(
-          "relative flex w-full touch-none select-none mb-6 items-center",
+          "relative flex w-full touch-none select-none mb-5 items-center",
           className,
         )}
         {...props}
       >
-        <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/20">
+        <SliderPrimitive.Track className="h-1 grow overflow-hidden rounded-full bg-primary/20">
           <SliderPrimitive.Range className="absolute h-full bg-primary" />
         </SliderPrimitive.Track>
         {localValues.map((value, index) => (
           <React.Fragment key={index}>
-            <div
-              className="absolute text-center"
-              style={{
-                left: `calc(${((value - min) / (max - min)) * 100}% + 0px)`,
-                top: `10px`,
-              }}
-            >
-              <span className="text-sm">
-                {formatLabel ? formatLabel(value) : value}
-              </span>
-            </div>
-            <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+            {!hideValue && (
+              <div
+                className="absolute text-center"
+                style={{
+                  left: `calc(${((value - min) / (max - min)) * 100}%)`, // Центрируем по thumb
+                  top: `15px`,
+                  transform: `translateX(${index === 0 ? "-100%" : "0"})`, // Левое значение сдвигаем влево
+                }}
+              >
+                <span className="text-sm">
+                  {formatLabel ? formatLabel(value) : value}
+                </span>
+              </div>
+            )}
+            <SliderPrimitive.Thumb className="block cursor-pointer hover:bg-primary h-4 w-4 rounded-full border border-primary bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
           </React.Fragment>
         ))}
       </SliderPrimitive.Root>
@@ -82,6 +88,6 @@ const RangeSlider = React.forwardRef(
   },
 );
 
-RangeSlider.displayName = SliderPrimitive.Root.displayName;
+RangeSlider.displayName = "RangeSlider";
 
 export { RangeSlider };
