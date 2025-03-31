61aea3d3d9be57191b5a9952bd495529dbe08743
Roman 4im <smartware10@gmail.com>
2025-03-31 13:32:17 +0300

    Create range-slider

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,6 @@ export { Button } from "./button";
 export { Checkbox } from "./checkbox";
 export { Input } from "./input";
 export { Slider } from "./slider";
+export { RangeSlider } from "./range-slider";
 export * from "./dropdown-menu";
 export * from "./popover";
