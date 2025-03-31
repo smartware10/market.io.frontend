f461a474264c20dbf8e30be2ad47837730319299
Roman 4im <smartware10@gmail.com>
2025-03-31 13:28:28 +0300

    Install slider to shadcn

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./button";
 export { Checkbox } from "./checkbox";
 export { Input } from "./input";
+export { Slider } from "./slider";
 export * from "./dropdown-menu";
 export * from "./popover";
