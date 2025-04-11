b16a6a8e2e8fc73c5124d5c9992ad0f40b2f20e1
Roman 4im <smartware10@gmail.com>
2025-04-11 15:38:59 +0300

    Upgrade checkbox-filters-group

--- src/components/shared/checkbox-filters-group.tsx
+++ src/components/shared/checkbox-filters-group.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import React from "react";
 import { FilterCheckbox, FilterCheckboxProps } from "@/components/shared";
 import { Input } from "@/components/ui";
+import { Minus, Plus, Search } from "lucide-react";
 
 type Item = FilterCheckboxProps;
 
 interface Props {
   title: string;
   items: Item[];
-  defaultItems?: Item[];
+  // defaultItems: Item[];
   limit?: number;
   searchInputPlaceholder?: string;
   onChange?: (values: string[]) => void;
@@ -18,38 +21,84 @@ interface Props {
 export const CheckboxFiltersGroup: React.FC<Props> = ({
   title,
   items,
-  defaultItems,
+  // defaultItems,
   limit = 5,
-  searchInputPlaceholder = "Поиск...",
+  searchInputPlaceholder = "Поиск",
   className,
   onChange,
   defaultValue,
 }) => {
+  const [showAll, setShowAll] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const list = showAll
+    ? items.filter((item) =>
+        item.text.toLowerCase().includes(searchValue.toLocaleLowerCase()),
+      )
+    : items.slice(0, limit);
+
   return (
     <>
       <div className={className}>
         <p className={"font-bold mb-3"}>{title}</p>
 
-        <div className={"ml-1 mr-1 mb-4"}>
-          <Input placeholder={searchInputPlaceholder} />
-        </div>
-
+        {showAll && (
+          <div className={"ml-1 mr-1 mb-4 relative"}>
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <Search size={20} className={"absolute text-primary"} />
+            </div>
+            <Input
+              value={searchValue}
+              onChange={onChangeSearchInput}
+              placeholder={searchInputPlaceholder}
+              className={"pl-9"}
+            />
+          </div>
+        )}
         <div
           className={
-            "flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar"
+            "flex flex-col gap-4 max-h-96 pr-2 overflow-y-auto custom-scrollbar"
           }
         >
-          {items.map((item, index) => (
+          {list.map((item) => (
             <FilterCheckbox
-              key={index}
+              key={item.value}
               text={item.text}
               value={item.value}
               endAdornment={item.endAdornment}
             />
           ))}
         </div>
+        {items.length > limit && (
+          <div className={showAll ? "border-t border-t-primary mt-4" : ""}>
+            <button
+              onClick={() => {
+                if (showAll) {
+                  setSearchValue("");
+                }
+                setShowAll(!showAll);
+              }}
+              className={"text-primary mt-3 ml-1 flex items-center"}
+            >
+              {showAll ? (
+                <>
+                  <Minus size={18} className={"mr-1"} />
+                  Скрыть
+                </>
+              ) : (
+                <>
+                  <Plus size={18} className={"mr-1"} />
+                  Показать всё
+                </>
+              )}
+            </button>
+          </div>
+        )}
       </div>
-      <div></div>
     </>
   );
 };
