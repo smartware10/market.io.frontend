362b4c4b33863a39318a2ced25c3c8c431056b9e
Roman 4im <smartware10@gmail.com>
2025-04-13 18:53:52 +0300

    Edit products-group-list

--- src/components/shared/products-group-list.tsx
+++ src/components/shared/products-group-list.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React from "react";
+import { useIntersection } from "react-use";
 import { Title, ProductCard } from "@/components/shared";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store";
 
 interface ProductCardProps {
   id: number;
@@ -25,23 +29,38 @@ export const ProductsGroupList: React.FC<Props> = ({
   listClassName,
   className,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const intersection = useIntersection(intersectionRef, { threshold: 0.4 });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting]);
   return (
     <>
-      <div className={className}>
+      <div className={className} id={title} ref={intersectionRef}>
         <Title text={title} size={"lg"} className={"font-extrabold mb-5"} />
 
-        <div className={cn("grid grid-cols-3 gap-11", listClassName)}>
-          {items.map((product) => (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              title={product.name}
-              description={product.description}
-              price={product.price}
-              imageUrl={product.imageUrl}
-            />
-          ))}
-        </div>
+        {items.length === 0 ? (
+          <p className="text-muted-foreground">Нет товаров в этой категории.</p>
+        ) : (
+          <div className={cn("grid grid-cols-3 gap-11", listClassName)}>
+            {items.map((product) => (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                title={product.name}
+                description={product.description}
+                price={product.price}
+                imageUrl={product.imageUrl}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
