43629d919f18e5ec420071b9efc7053c9ed2e9df
Roman 4im <smartware10@gmail.com>
2025-04-13 16:24:15 +0300

    Using product group list and add img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {
   Filters,
   Title,
   TopBar,
-  ProductCard,
+  ProductsGroupList,
 } from "@/components/shared";
 
 export default function Home() {
@@ -16,28 +16,141 @@ export default function Home() {
       <TopBar />
 
       <Container className={"mt-5 pb-14"}>
-        <div className={"flex gap-[60px]"}>
+        <div className={"flex gap-[40px]"}>
           <div className={"w-[250px]"}>
             <Filters />
           </div>
 
           <div className={"flex-1"}>
             <div className={"flex flex-col gap-16"}>
-              <ProductCard
-                id={1}
-                title={"Компьютер"}
-                description={
-                  'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой'
-                }
-                imageUrl={"/img.png"}
-                price={5500}
+              <ProductsGroupList
+                title={"Компьютеры"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 500,
+                  },
+                  {
+                    id: 2,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 15500,
+                  },
+                  {
+                    id: 3,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 5660,
+                  },
+                  {
+                    id: 4,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 100,
+                  },
+                  {
+                    id: 5,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 8700,
+                  },
+                  {
+                    id: 6,
+                    name: "Компьютер",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img.png",
+                    price: 5500,
+                  },
+                ]}
+                categoryId={1}
+              />
+
+              <ProductsGroupList
+                title={"Планшеты"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Планшет",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_1.png",
+                    price: 500,
+                  },
+                  {
+                    id: 2,
+                    name: "Планшет",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_1.png",
+                    price: 15500,
+                  },
+                  {
+                    id: 3,
+                    name: "Планшет",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_1.png",
+                    price: 5660,
+                  },
+                ]}
+                categoryId={2}
+              />
+
+              <ProductsGroupList
+                title={"Смартфоны"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Смартфон",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_2.png",
+                    price: 500,
+                  },
+                  {
+                    id: 2,
+                    name: "Смартфон",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_2.png",
+                    price: 15500,
+                  },
+                  {
+                    id: 3,
+                    name: "Смартфон",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_2.png",
+                    price: 5660,
+                  },
+                  {
+                    id: 4,
+                    name: "Смартфон",
+                    description:
+                      'Экран 13.6" Liquid Retina (2560x1664), глянцевый / Apple M4 / RAM 16 ГБ / SSD 256 ГБ / Apple M4 Graphics (8 ядер) / Wi-Fi / Bluetooth / macOS Sequoia / 1.24 кг / голубой',
+                    imageUrl: "/img_2.png",
+                    price: 100,
+                  },
+                ]}
+                categoryId={3}
               />
             </div>
           </div>
         </div>
       </Container>
-
-      <div className={"h-[3000px]"} />
     </>
   );
 }
